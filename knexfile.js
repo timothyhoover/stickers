d1a53aed15796ba8818d917e48286f3f9b37ab77
timothyhoover <timmchoover@gmail.com>
2021-06-28 12:02:00 -0500

    updating config knex

--- knexfile.js
+++ knexfile.js
@@ -7,6 +7,11 @@ module.exports = {
 	},
 	production: {
 		client: 'pg',
-		connection: process.env.DATABASE_URL,
+		connection: {
+			connectionString: process.env.DATABASE_URL,
+			ssl: {
+				rejectUnauthorized: false,
+			},
+		},
 	},
 };
