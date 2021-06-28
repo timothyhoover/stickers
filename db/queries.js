312fe520859a7760287ec1f15badaca662b7339a
timothyhoover <timmchoover@gmail.com>
2021-06-28 11:25:00 -0500

    first commit

---
+++ db/queries.js
@@ -0,0 +1,29 @@
+const knex = require('./knex'); // the connection
+
+module.exports = {
+	getAll(query) {
+		const knexQuery = knex('sticker');
+
+		if (query.title) {
+			knexQuery.whereRaw(`title like %${query.title}%`);
+		}
+
+		if (query.description) {
+			knexQuery.where('description', 'like', `%${query.description}%`);
+		}
+
+		return knexQuery;
+	},
+	getOne(id) {
+		return knex('sticker').where('id', id).first();
+	},
+	create(sticker) {
+		return knex('sticker').insert(sticker, '*');
+	},
+	update(id, sticker) {
+		return knex('sticker').where('id', id).update(sticker, '*');
+	},
+	delete(id) {
+		return knex('sticker').where('id', id).del();
+	},
+};
---
+++ seeds/01_sticker.js
@@ -0,0 +1,9 @@
+const stickers = require('../stickers');
+
+exports.seed = function (knex, Promise) {
+	return knex('sticker')
+		.del()
+		.then(function () {
+			return knex('sticker').insert(stickers);
+		});
+};
